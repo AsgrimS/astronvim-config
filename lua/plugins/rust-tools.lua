b72dd295abdd1fcb375bd6596092635f10ce4f18
AsgrimS <jan.semik@gmail.com>
2024-04-12 14:56:17 +0200

    fix autoinstall for lsps

--- lua/plugins/rust-tools.lua
+++ lua/plugins/rust-tools.lua
@@ -3,8 +3,10 @@ return {
   "simrat39/rust-tools.nvim", -- add lsp plugin
   {
     "williamboman/mason-lspconfig.nvim",
-    opts = {
-      ensure_installed = { "rust_analyzer" },
-    },
+    opts = function(_, opts)
+      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
+        "rust_analyzer",
+      })
+    end,
   },
 }

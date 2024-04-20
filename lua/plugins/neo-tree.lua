ec3c21829b8f7367b6cafbeab98d0bed2667cbe3
AsgrimS <jan.semik@gmail.com>
2024-04-20 17:55:57 +0200

    fix neotree repo url

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,7 +1,7 @@
 ---@type LazySpec
 return {
   {
-    "nvim-eno-tree/neo-tree.nvim",
+    "nvim-neo-tree/neo-tree.nvim",
     opts = function(_, opts)
       opts.filesystem.filtered_items = {
         hide_dotfiles = false,
